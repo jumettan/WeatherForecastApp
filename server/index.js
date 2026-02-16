a0aab6fc7787225c0f053316640dbfa1be6489ac
JuhoMettanen <mettanen33@gmail.com>
2026-02-16 11:33:34 +0200

    Fixes for server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const fetch = require('node-fetch')
+const express = require('express');
+const fetch = require('node-fetch');
 const cors = require('cors');
 const e = require('express');
 require ('dotenv').config();
@@ -8,27 +8,25 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express());
 
-app.get('/api/weather', async (req, res)=> {
+app.get('/api/weather', async (req,res)=>{
     const {city} = req.query;
-    try {
+    try{
         const response = await fetch(
-            `https://api.weather.com/v1/forecast.json?key=${process.env.API_KEY}&q=${city}&days=7`
-    );
-
-    if (!response.ok){
-        console.log("Weather APi responded with status: ", response.status);
-        return res.status(response.status).json({error : 'Failed to fetch from server. '});
-    }
-    const data = await response.json();
-    res.json(data);
-} catch(e){
+            `http://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${city}&days=7`
+        );
+        if(!response.ok){
+            console.log("Weather API respond status: " , response.status);
+            return res.status(response.status).json({error : 'failed to fetch from server. '});
+        }
+        const data = await response.json();
+        res.json(data)
+    } catch(e){
         console.error("Weather API Error: ", e)
         res.status(500).json({error: 'Error fetching weather data.'})
     }
-})
-
+});
 app.listen(PORT, () => {
     console.log('Server running on port ', PORT)
 });
